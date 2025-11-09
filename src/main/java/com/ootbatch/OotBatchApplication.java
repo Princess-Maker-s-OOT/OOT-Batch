a60f278b443f924396f34906c9d6d102d2a05754
byeongsu-cmd <byeongsukim0607@gmail.com>
2025-11-09 17:42:54 +0900

    초기 batch 프로젝트 구조 생성

---
+++ src/main/java/com/ootbatch/OotBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ootbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OotBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OotBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ootbatch/OotBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ootbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OotBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
