bff61a2d1c07ede23f05b23e0831e03534ca7114
zerone <bazz0324@naver.com>
2025-11-16 19:22:45 +0900

    Revert "[feat] 추천 생성 Job 구성 및 Internal API 연동 구현"

--- src/main/java/com/ootbatch/OotBatchApplication.java
+++ src/main/java/com/ootbatch/OotBatchApplication.java
@@ -2,22 +2,14 @@ package com.ootbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EntityScan(basePackages = {
-        "com.ootbatch",                     // 배치 서버 엔티티
-        "com.ootcommon.recommendation"      // 공통 모듈 엔티티
-})
-@EnableJpaRepositories(basePackages = {
-        "com.ootbatch",                     // 배치 서버 레포지토리
-})
 public class OotBatchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OotBatchApplication.class, args);
     }
+
 }
